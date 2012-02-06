7e60f9473f98015d172ed3029258a119433f8703
adrian.veith@gmail.com <adrian.veith@gmail.com@3cf17c20-d594-11dd-a3c7-fda619b15ae3>
2012-02-06 11:25:10 +0000

    moved frac, trunc and round to MathX.hx small changes

--- trunk/p4n/Tools.hx
+++ trunk/p4n/Tools.hx
@@ -1,13 +1,29 @@
-﻿/**
- * ...
- * @author ...
- */
+﻿/**************************************************************************************************
+{                                                                                                  
+{ The contents of this file are subject to the Mozilla Public License Version 1.1 (the "License"); 
+{ you may not use this file except in compliance with the License. You may obtain a copy of the    
+{ License at http://www.mozilla.org/MPL/                                                           
+{                                                                                                  
+{ Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF   
+{ ANY KIND, either express or implied. See the License for the specific language governing rights  
+{ and limitations under the License.                                                               
+{                                                                                                  
+{ The Original Code is Tools.hx.                                                                   
+{                                                                                                  
+{ The Initial Developer of the Original Code is Adrian Veith <adrian@veith-system.de>.             
+{ Portions created by Adrian Veith Copyright (C) Adrian Veith. All Rights Reserved.                
+{                                                                                                  
+{ Contributors:                                                                                    
+{   Adrian Veith                                                                                   
+{                                                                                                  
+{**************************************************************************************************/
 
 package p4n;
 
 #if neko	
 import neko.Lib;
 import neko.NativeString;
+import p4n.MathX;
 
 class Globals implements Dynamic < Dynamic > {
 	private var g: Dynamic;
@@ -24,9 +40,51 @@ class Globals implements Dynamic < Dynamic > {
 }
 #end
 
+
 class Tools 
 {
 #if neko	
+	static var id__reference__: Int;
+	public static function exportObject(o: Dynamic): Dynamic {
+		if (o == null) return null;
+		untyped {
+			var f = __dollar__objfields(o);
+			var i = 0;
+			var l = __dollar__asize(f);
+			while( i < l ) {
+				var v = __dollar__objget(o, f[i]);
+				switch (__dollar__typeof(v)) {
+					case __dollar__tstring: {
+						var s = neko.Utf8.encode(new String(v));
+						__dollar__objset(o, f[i], s);
+					}
+					case __dollar__tabstract: __dollar__objset(o, f[i], null); 
+					case __dollar__tobject: {
+						if (v.__s != null)
+							__dollar__objset(o, f[i], neko.Utf8.encode(v));
+					}
+					case __dollar__tarray: {
+						var a = neko.NativeArray.toArray(o); 
+						for (i in 0...a.length) exportObject(a[i]); 
+						__dollar__objset(o, f[i], a);
+					}
+					default: {}
+				}
+				i = i + 1;
+			}
+		}
+		return o;
+	}
+	public static function exportList<T>(AList: List<T>) 
+	{
+		
+		if (AList == null) return null;
+		for (x in AList) { 
+			x = exportObject(x);
+		}
+		return AList;
+	}
+	
 
 	static public function toHaXe(o: Dynamic): Dynamic {
 		
@@ -41,22 +99,29 @@ class Tools
 		if (o == null) return null;
 		untyped {
 			var f = __dollar__objfields(o);
+			//trace(f);
 			var i = 0;
 			var l = __dollar__asize(f);
-			while( i < l ) {
-				var v = __dollar__objget(o, f[i]);
+			while ( i < l ) {
+				var fx = f[i];
+				if (stripAbstract && fx == id__reference__) {
+					__dollar__objremove(o, fx);
+					i++;
+					continue;
+				}
+				var v = __dollar__objget(o, fx);
 				switch (__dollar__typeof(v)) {
 					case __dollar__tstring:
 						if (toUtf == null || !toUtf)
-							__dollar__objset(o, f[i], new String(v));
+							__dollar__objset(o, fx, new String(v));
 						else {
 							var s = neko.Utf8.encode(new String(v));
-							__dollar__objset(o, f[i], s);
+							__dollar__objset(o, fx, s);
 						}
-					case __dollar__tabstract: if (stripAbstract == true) {__dollar__objset(o, f[i], null); }
+					case __dollar__tabstract: if (stripAbstract == true) {__dollar__objremove(o, fx); }
 					default: {}
 				}
-				i = i + 1;
+				i++;
 			}
 		}
 		if (proto != null) {
@@ -69,7 +134,12 @@ class Tools
 	static var _app_terminated;
 	static var _get_global;
 	static var _app_path;
-  static public function __init__() {
+	static public function __init__() {
+		untyped {
+			var temp = { __reference__: 0 };
+			var f = __dollar__objfields(temp);
+			id__reference__ = f[0];
+		}
 		try {
 			_app_terminated = Lib.load('p4n', 'app_terminated', 0);
 			_app_path = Lib.load('p4n', 'app_path', 0);
@@ -92,26 +162,10 @@ class Tools
 	static public function AppPath(): String { return if (_app_path != null) NativeString.toString(_app_path()) else ""; }
 
 #end
-	static public inline function frac(value: Float): Float {
-		return if (value >= 0) value - Math.floor(value) else value - Math.ceil(value);
-	}
-	static public inline function trunc(value: Float): Float {
-		return if (value >= 0) Math.floor(value) else Math.ceil(value);
-	}
-	
-	static var stellen = [0.00000001,0.0000001,0.000001,0.00001,0.0001,0.001,0.01,0.1,
-		1.0, 10.0, 100.0, 1000.0, 10000.0, 100000.0, 1000000.0, 10000000.0, 100000000.0];
-		
-	static public function round(x: Float, n: Int): Float {
-		if ((n > 8) || (n < -8)) {
-			return x;
-		} else {
-			var sf = stellen[n + 8];
-			var xx = x * sf;
-			return (trunc(xx) + trunc(frac(xx) * 2)) / sf;
-		}
-	}
-	
+	static public inline function frac(value: Float): Float { return MathX.frac(value); }
+	static public inline function trunc(value: Float): Float { return MathX.trunc(value); }		
+	static public inline function round(x: Float, n: Int): Float { return MathX.round(x, n); }
+
 	static public inline function def < T > (Value: T, DefValue: T): T { return if (Value != null) Value else DefValue; }
 	static public function IncludeTrailingPathDelimter(path: String): String {
 		var c1 = path.lastIndexOf("/");
